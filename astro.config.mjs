6d7ad7ad83c5a840e3385abafb2eb36b9f0c718c
Alec Jensen <alecbradyjensen@gmail.com>
2024-02-21 13:12:29 -0600

    add search search to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		pagefind()
+	],
 });
