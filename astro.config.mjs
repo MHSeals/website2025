bb0f5745b0b9f4698aecd4d2aa5e0d2566ac6c92
Alec Jensen <59067840+alec-jensen@users.noreply.github.com>
2024-03-03 15:23:50 -0600

    add robots.txt, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mhsroboboat.com',
 	integrations: [
 		mdx(),
 		sitemap(),
