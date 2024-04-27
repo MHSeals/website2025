dd37f38b999a657ac7753248fa5e5655f5a53ead
Alec Jensen <alecbradyjensen@gmail.com>
2024-04-26 20:57:49 -0500

    update home page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'MHS Seals';
-export const SITE_DESCRIPTION = 'James Martin High School Roboboat Team';
+export const SITE_DESCRIPTION = 'MHS Seals Roboboat Team';
